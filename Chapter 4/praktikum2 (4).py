0e40d23b41a56088767d770ac9d3d94e33c46668
riyanbagasdp <riyanbagasdp@student.uns.ac.id>
2021-10-12 20:56:09 +0700

    Create praktikum2 (4).py

---
+++ Chapter 4/praktikum2 (4).py
@@ -0,0 +1,249 @@
+print ('int + int')
+x = 10
+print(type(x))
+y = 20
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('int + float')
+x = 10
+print(type(x))
+y = 2.1
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('float + int')
+x = 1.2
+print(type(x))
+y = 3
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('float + float')
+x = 1.2
+print(type(x))
+y = 2.1
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('int - int')
+x = 3
+print(type(x))
+y = 2
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('int - float')
+x = 3
+print(type(x))
+y = 2.2
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('float - int')
+x = 3.2
+print(type(x))
+y = 2
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('float - float')
+x = 3.2
+print(type(x))
+y = 2.2
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('int * int')
+x = 3
+print(type(x))
+y = 2
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('int * float')
+x = 3
+print(type(x))
+y = 2.2
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('float * int')
+x = 3.2
+print(type(x))
+y = 2
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('float * float')
+x = 3.2
+print(type(x))
+y = 2.2
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('int / int')
+x = 3
+print(type(x))
+y = 2
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('int / float')
+x = 3
+print(type(x))
+y = 2.2
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('float / int')
+x = 3.2
+print(type(x))
+y = 2
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('float / float')
+x = 3.2
+print(type(x))
+y = 2.2
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('int // int')
+x = 3
+print(type(x))
+y = 2
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('int // float')
+x = 3
+print(type(x))
+y = 2.2
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('float // int')
+x = 3.2
+print(type(x))
+y = 2
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('float // float')
+x = 3.2
+print(type(x))
+y = 2.2
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('int % int')
+x = 3
+print(type(x))
+y = 2
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('int % float')
+x = 3
+print(type(x))
+y = 2.2
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('float % int')
+x = 3.2
+print(type(x))
+y = 2
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('float % float')
+x = 3.2
+print(type(x))
+y = 2.2
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('int ** int')
+x = 3
+print(type(x))
+y = 2
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('int ** float')
+x = 3
+print(type(x))
+y = 2.2
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('float ** int')
+x = 3.2
+print(type(x))
+y = 2
+print(type(y))
+print(type(x+y))
+
+print (' ')
+
+print ('float ** float')
+x = 3.2
+print(type(x))
+y = 2.2
+print(type(y))
+print(type(x+y))
