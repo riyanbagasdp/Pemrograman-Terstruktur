7556eb3d4dfe9e28929b41fbfd5b7bba76ef781a
riyanbagasdp <riyanbagasdp@student.uns.ac.id>
2021-10-12 20:55:35 +0700

    Create latihan (1).py

---
+++ Chapter 4/latihan (1).py
@@ -0,0 +1,12 @@
+tarifduabelasjam = 200000
+tarifperjamlanjut = 10000
+berangkat = 600
+pulang = 2350
+jam = (pulang-berangkat)
+print (jam)
+
+jamsisa = (1750-1200)
+jamsisamenit = (5*60+50)
+tarifpermenit = (10000/60)
+totaltarif = 200000 + (tarifpermenit)*350
+print (totaltarif)
