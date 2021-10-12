bc93de9757266709009832ea504bfee8819ecd62
riyanbagasdp <riyanbagasdp@student.uns.ac.id>
2021-10-12 20:55:47 +0700

    Create latihan (5).py

---
+++ Chapter 4/latihan (5).py
@@ -0,0 +1,5 @@
+mhslaki = int(input("jumlah mahasiswa laki-laki PTIK?"))
+mhsperempuan = int(input("jumlah mahasiswa perempuan PTIK21?"))
+print("Laki-laki =" + mhslaki*"|")
+print("Perempuan =" + mhsperempuan*"|")
+
