23f0418b38af69b86be48f0bb4756b0da2942805
riyanbagasdp <riyanbagasdp@student.uns.ac.id>
2021-11-01 12:20:14 +0700

    Create prak 6.2.8.py

---
+++ Chapter 6/prak 6.2.8.py
@@ -0,0 +1,4 @@
+def perkenalan(nama, usia, asal):
+    print('Halo perkenalkan namaku', nama, 'usiaku sekarang adalah', usia, 'dan aku berasal dari', asal)
+
+perkenalan('Riyan', '18 Tahun', 'Surakarta')
