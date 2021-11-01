83aad4f716b80860c597e041a676c9d458b0e203
riyanbagasdp <riyanbagasdp@student.uns.ac.id>
2021-11-01 11:59:03 +0700

    Create chapter5 lat1.py

---
+++ Chapter 5/chapter5 lat1.py
@@ -0,0 +1,11 @@
+nilaiBhsIndo = 55
+print("Masukkan nilai Bhs Indonesia :" + str(nilaiBhsIndo))
+nilaiIPA = 92
+print("Masukkan nilai IPA           :" + str(nilaiIPA))
+nilaiMAT = 87
+print("Masukkan nilai Matematika    :"  + str(nilaiMAT))
+print (" ")
+if (nilaiIPA >= 60) and (nilaiMAT > 70) and (nilaiBhsIndo >= 60):
+    print ("Status Kelulusan             : LULUS")
+else :
+     print ("Status Kelulusan             : TIDAK LULUS")
