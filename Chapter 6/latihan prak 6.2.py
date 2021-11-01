e99f891509e917341b995f907ac1384ce2f9755a
riyanbagasdp <riyanbagasdp@student.uns.ac.id>
2021-11-01 12:19:40 +0700

    Create latihan prak 6.2.py

---
+++ Chapter 6/latihan prak 6.2.py
@@ -0,0 +1,37 @@
+def starFormation1(n):
+    kolom = 1
+    i = 0
+    while(i < n):
+        j = 0
+        while(j < kolom):
+            print('* ', end='')
+            j += 1
+        kolom += 1
+        print('')
+        i += 1
+
+def starFormation2(n):
+    kolom = n
+    i = 0
+    while(i < n):
+        j = 0
+        while(j < kolom):
+            print('* ', end='')
+            j += 1
+        kolom -= 1
+        print('')
+        i += 1
+
+def starFormation3(n):
+    if(n % 2 == 0): 
+        # Jika Genap
+        starFormation1(n//2)
+    else:
+        # Jika Ganjil
+        starFormation1(n//2 + 1)
+    starFormation2(n//2)
+    
+    
+starFormation1(4)
+starFormation2(4)
+starFormation3(7)
