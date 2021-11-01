b64221ebaba814624c9b3aea755d930b1868f050
riyanbagasdp <riyanbagasdp@student.uns.ac.id>
2021-11-01 12:20:06 +0700

    Create prak 6.2.1.py

---
+++ Chapter 6/prak 6.2.1.py
@@ -0,0 +1,11 @@
+def luasSegitiga(alas, tinggi):
+    luas = alas * tinggi / 2
+    return luas
+
+alas = 10
+tinggi = 20
+print('Luas segitiga dg alas ', alas,' dan tinggi ', tinggi,' adalah ', luasSegitiga(alas, tinggi))
+
+a = 15
+t = 45
+print('Luas segitiga dg alas ', a,' dan tinggi ', t,' adalah ', luasSegitiga(a, t))
