14ccd17a08d32041892078466dd124205b1423a8
riyanbagasdp <riyanbagasdp@student.uns.ac.id>
2021-11-01 12:19:52 +0700

    Create latihan prak 6.5db.py

---
+++ Chapter 6/latihan prak 6.5db.py
@@ -0,0 +1,11 @@
+from average import *
+
+function(81, 98, 12, 83, 45, 77, 69, 30, 56)
+
+from max import *
+
+function1(81, 98, 12, 83, 45, 77, 69, 30, 56)
+
+from min import *
+
+function1(81, 98, 12, 83, 45, 77, 69, 30, 56)
