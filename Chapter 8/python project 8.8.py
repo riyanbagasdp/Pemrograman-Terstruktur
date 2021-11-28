0737188624a15bec208390969c58abda1ddd3ed3
riyanbagasdp <riyanbagasdp@student.uns.ac.id>
2021-11-28 20:56:25 +0700

    Create python project 8.8.py

---
+++ Chapter 8/python project 8.8.py
@@ -0,0 +1,12 @@
+buah = {'apel' : 5000, 'jeruk' : 8500, 'mangga' : 7800, 'duku' : 6500}
+
+# mengkonversi values dictionary ke list
+data = list(buah.values()) 
+buahan=[data]
+x = (buahan[0])
+a = len(x)
+b = sum(x)
+
+hasil = b/a
+print(hasil)
+
