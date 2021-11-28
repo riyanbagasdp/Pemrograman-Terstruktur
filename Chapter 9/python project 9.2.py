c50ac08ac77e8fd3b1d3ce0718c50bf307273a5f
riyanbagasdp <riyanbagasdp@student.uns.ac.id>
2021-11-28 21:00:26 +0700

    Create python project 9.2.py

---
+++ Chapter 9/python project 9.2.py
@@ -0,0 +1,9 @@
+def bintang(n):
+    space = 2*n-1
+    for i in range(n):
+        print(('*'*(2*i+ 1)).center(space))
+def bintang2(n):
+    for i in range(n):
+        print('*'*())
+        
+bintang(4)
