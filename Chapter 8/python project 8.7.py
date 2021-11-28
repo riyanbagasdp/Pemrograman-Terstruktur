7785cfa3842be3d89a40625dbb92d4f4b3d26c16
riyanbagasdp <riyanbagasdp@student.uns.ac.id>
2021-11-28 20:56:22 +0700

    Create python project 8.7.py

---
+++ Chapter 8/python project 8.7.py
@@ -0,0 +1,18 @@
+buah = {'apel' : 5000, 'jeruk' : 8500, 'mangga' : 7800, 'duku' : 6500}
+
+# mengkonversi values dictionary ke list
+data = list(buah.values()) 
+# lakukan sorting 
+data.sort()
+buahan=[data]
+x = (buahan[0])
+print(x)
+print(x[3])
+
+
+
+
+
+
+
+
