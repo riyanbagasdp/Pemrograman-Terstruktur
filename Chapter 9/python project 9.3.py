5399731ad898a06a2600bf9685b08a5c16752de6
riyanbagasdp <riyanbagasdp@student.uns.ac.id>
2021-11-28 21:00:30 +0700

    Create python project 9.3.py

---
+++ Chapter 9/python project 9.3.py
@@ -0,0 +1,10 @@
+def bintang(n):    
+    num = n
+
+    for i in range(1, num+1):
+        i = i - (num//2 +1)
+        if i < 0:
+            i = -i
+        print(" " * i + "*" * (num - i*2) + " "*i)
+
+bintang(7)
