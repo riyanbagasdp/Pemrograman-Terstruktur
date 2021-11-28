edeed668fd6ded1a3ae090cf5b657949bd13c9de
riyanbagasdp <riyanbagasdp@student.uns.ac.id>
2021-11-28 20:54:02 +0700

    Create praktikum 3.5.py

---
+++ Chapter 7/praktikum 3.5.py
@@ -0,0 +1,11 @@
+def menghitung2():
+    try:
+        file = open("d:/data.txt", "r")
+        sum = 0
+        for data in file:
+            sum = sum + int(data)
+        print(sum)
+    except ValueError:
+        print('Input data bukan angka')
+
+menghitung2()
